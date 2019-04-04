6318ccf288bc164ddf443c37015231700cfcef70
Nico Rehwaldt <git_nikku@nixis.de>
2019-04-04 22:33:47 +0200

    chore(app): allow PORT and HOST to be defined in prod build

--- razzle.config.js
+++ razzle.config.js
@@ -4,6 +4,19 @@ module.exports = {
 
     delete config.externals;
 
+    if (target === 'node' && !dev) {
+
+      // remove define plug-in definitions
+      const definePlugin = config.plugins[0];
+
+      Object.keys(definePlugin.definitions).forEach(key => {
+        if (key === 'process.env.PORT' || key === 'process.env.HOST') {
+          definePlugin.definitions[key] = key;
+        }
+      });
+
+    }
+
     return config;
-  },
+  }
 };
