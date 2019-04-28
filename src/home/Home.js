f1a4367cdf15d7b4f2754dfe6605eae2a66bdcf4
Nico Rehwaldt <git_nikku@nixis.de>
2019-04-28 21:02:05 +0200

    feat(app): add landing page

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,31 +1,91 @@
 import React from 'react';
-import logo from './react.svg';
-import './Home.css';
+
+import classNames from 'classnames';
+
+import {
+  Layout,
+  Menu,
+  Button
+} from 'antd';
+
+import css from './Home.less';
+
+const {
+  Header,
+  Content,
+  Footer
+} = Layout;
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="Home">
-        <div className="Home-header">
-          <img src={ logo } className="Home-logo" alt="logo" />
-          <h2>Welcome to Razzle</h2>
-        </div>
-        <p className="Home-intro">
-          To get started, edit <code>src/App.js</code> or{' '}
-          <code>src/Home.js</code> and save to reload.
-        </p>
-        <ul className="Home-resources">
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-          </li>
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </li>
-          <li>
-            <a href="https://palmer.chat">Community Slack</a>
-          </li>
-        </ul>
-      </div>
+      <Layout className={ classNames(css.Home, 'layout') }>
+        <Header>
+          <div className="logo">
+          </div>
+
+          <Menu
+            theme="light"
+            mode="horizontal"
+            className="menu"
+          >
+            <Menu.Item key="features"><a href="#features">Features</a></Menu.Item>
+            <Menu.Item key="github" className="menu-right"><a href="https://github.com/nikku/wuffle" target="_blank">GitHub</a></Menu.Item>
+          </Menu>
+        </Header>
+        <Content className="content">
+
+          <div className="well">
+            <img src="/favicon.svg" width="155" className="logo" />
+
+            <h1>Track all your GitHub projects on a unified task board.</h1>
+
+            <Button href="./board" size="large" type="primary">
+              See Board
+            </Button>
+
+            &nbsp; or <a href="https://github.com/nikku/wuffle">view on GitHub</a>.
+          </div>
+
+
+          <a id="features" className="section-link">features</a>
+
+          <section>
+            <h1>Maps your Development Cycle to Columns</h1>
+
+            <p>
+
+            </p>
+          </section>
+
+
+          <section>
+            <h1>Tight integration with the <a href="https://guides.github.com/introduction/flow/">GitHub flow</a></h1>
+
+            <p>
+            </p>
+          </section>
+
+
+          <section>
+            <h1>Publicly accessible</h1>
+
+            <p>
+            </p>
+          </section>
+
+
+          <section>
+            <h1>Open Source and Self-Hostable.</h1>
+          </section>
+
+        </Content>
+
+        <Footer className="footer">
+          Wuffle is a project by Nico Rehwaldt © 2019 / <a href="https://github.com/nikku/wuffle">view on GitHub</a>
+        </Footer>
+
+      </Layout>
     );
   }
 }
