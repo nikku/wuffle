47ef10f8acea38fdd4490ad1ad054beb38516eb8
Nico Rehwaldt <git_nikku@nixis.de>
2019-07-11 19:59:00 +0200

    fix(board): correct closed depends-in link color

--- packages/board/src/components/LinkIcon.svelte
+++ packages/board/src/components/LinkIcon.svelte
@@ -41,7 +41,9 @@
 {#if name === 'depends-on'}
 
   {#if state === 'closed'}
-    <svg class="icon depends-on closed { className }" width="1rem" height="1rem" viewBox="0 0 12 16" version="1.1" aria-hidden="true"><path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path></svg>
+    <svg class="icon depends-on closed { className }" width="1rem" height="1rem" viewBox="0 0 12 16" version="1.1" aria-hidden="true" fill="currentColor">
+      <path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path>
+    </svg>
   {:else}
     <svg class="icon depends-on { className }" width="1rem" height="1rem" viewBox="0 0 14 16" version="1.1" aria-hidden="true" fill="currentColor">
       <path fill-rule="evenodd" d="M7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm0 1.3c1.3 0 2.5.44 3.47 1.17l-8 8A5.755 5.755 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zm0 11.41c-1.3 0-2.5-.44-3.47-1.17l8-8c.73.97 1.17 2.17 1.17 3.47 0 3.14-2.56 5.7-5.7 5.7z"></path>
