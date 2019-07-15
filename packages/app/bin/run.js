1231c15df383da34cc1c8fc8fc9f1ddaa85cee6a
Nico Rehwaldt <git_nikku@nixis.de>
2019-07-15 16:10:01 +0200

    chore(app): improve run script
    * async validate and run
    * print started URL

--- packages/app/bin/run.js
+++ packages/app/bin/run.js
@@ -13,114 +13,142 @@ const log = wrapLogger(logger, logger).child({
 
 const { version } = require('../package');
 
-// validate ///////////
-
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const IS_PROD = NODE_ENV === 'production';
+const IS_DEV = NODE_ENV === 'development';
 
-log.info('Validating configuration');
 
-const problems = [
-  checkEnv('APP_ID', IS_PROD),
-  checkEnv('PRIVATE_KEY', IS_PROD),
-  checkEnv('WEBHOOK_SECRET', IS_PROD),
-  checkEnv('GITHUB_CLIENT_ID', IS_PROD),
-  checkEnv('GITHUB_CLIENT_SECRET', IS_PROD),
-  checkEnv('SESSION_SECRET', IS_PROD),
-  checkEnv('BASE_URL', IS_PROD),
-  checkConfig(),
-  checkBaseUrl()
-].filter(problem => problem);
+async function validate() {
 
-function isFatal(problem) {
-  return problem.type === 'ERROR';
-}
+  log.info('Validating configuration');
 
-function checkBaseUrl() {
+  const problems = [
+    checkEnv('APP_ID', IS_PROD),
+    checkEnv('PRIVATE_KEY', IS_PROD),
+    checkEnv('WEBHOOK_SECRET', IS_PROD),
+    checkEnv('GITHUB_CLIENT_ID', IS_PROD),
+    checkEnv('GITHUB_CLIENT_SECRET', IS_PROD),
+    checkEnv('SESSION_SECRET', IS_PROD),
+    checkEnv('BASE_URL', IS_PROD),
+    checkConfig(),
+    checkBaseUrl()
+  ].filter(problem => problem);
 
-  const baseUrl = process.env.BASE_URL;
+  function isFatal(problem) {
+    return problem.type === 'ERROR';
+  }
 
-  if (baseUrl) {
-    if (baseUrl.includes('/board')) {
-      return error('env.BASE_URL must point to the root of the webapp');
-    }
+  function checkBaseUrl() {
 
-    if (!baseUrl.startsWith('http')) {
-      return error('env.BASE_URL must start with protocol (http/https)');
-    }
+    const baseUrl = process.env.BASE_URL;
 
-    if (baseUrl.endsWith('/')) {
-      return error('env.BASE_URL must not end with trailing slash');
+    if (baseUrl) {
+      if (baseUrl.includes('/board')) {
+        return error('env.BASE_URL must point to the root of the webapp');
+      }
+
+      if (!baseUrl.startsWith('http')) {
+        return error('env.BASE_URL must start with protocol (http/https)');
+      }
+
+      if (baseUrl.endsWith('/')) {
+        return error('env.BASE_URL must not end with trailing slash');
+      }
     }
   }
-}
 
-function checkConfig() {
+  function checkConfig() {
 
-  if (process.env.BOARD_CONFIG) {
+    const problem = IS_PROD ? error : warning;
+
+    if (process.env.BOARD_CONFIG) {
+      try {
+        JSON.parse(process.env.BOARD_CONFIG);
+
+        // we cool!
+        return;
+      } catch (err) {
+        return problem(`Failed to parse env.BOARD_CONFIG as JSON: ${err.message}`);
+      }
+    }
+
     try {
-      JSON.parse(process.env.BOARD_CONFIG);
-
-      // we cool!
-      return;
+      require('../wuffle.config.js');
     } catch (err) {
-      return error(`Failed to parse env.BOARD_CONFIG as JSON: ${err.message}`);
+      return problem('Board not configured via env.BOARD_CONFIG or wuffle.config.js');
+    }
+
+  }
+
+  function checkEnv(key, isError=false) {
+
+    if (!process.env[key]) {
+      return (isError ? error : warning)(
+        `Missing env.${key}`
+      );
     }
   }
 
-  try {
-    require('../wuffle.config.js');
-  } catch (err) {
-    return error('Board not configured via env.BOARD_CONFIG or wuffle.config.js');
+  function error(message) {
+    return {
+      type: 'ERROR',
+      message
+    };
   }
 
-}
-
-function checkEnv(key, isError=false) {
-
-  if (!process.env[key]) {
-    return (isError ? error : warning)(
-      `Missing env.${key}`
-    );
-  }
-}
-
-function error(message) {
-  return {
-    type: 'ERROR',
-    message
-  };
-}
-
-function warning(message) {
-  return {
-    type: 'WARN',
-    message
-  };
-}
-
-const fatal = problems.some(isFatal);
-
-if (problems.length) {
-
-  for (const problem of problems) {
-    log[isFatal(problem) ? 'error' : 'warn'](problem.message);
+  function warning(message) {
+    return {
+      type: 'WARN',
+      message
+    };
   }
 
-  log.error('Please refer to https://wuffle.dev for setup instructions');
+  const fatal = problems.some(isFatal);
 
-  if (fatal) {
-    log.error('Exiting due to error(s)');
-    process.exit(1);
+  if (problems.length) {
+
+    for (const problem of problems) {
+      log[isFatal(problem) ? 'error' : 'warn'](problem.message);
+    }
+
+    log[fatal ? 'error' : 'warn']('Please refer to https://wuffle.dev for setup instructions');
+
+    if (fatal) {
+      log.error('Exiting due to error(s)');
+      process.exit(1);
+    }
   }
 }
 
+async function start() {
 
-// run /////////
+  log.info(`Starting Wuffle v${version}`);
 
-log.info(`Starting Wuffle v${version}`);
+  const app = require('../');
 
-const app = require('../');
+  await Probot.run(app);
+}
 
-Probot.run(app);
+async function open() {
+
+  if (IS_DEV) {
+    const url = process.env.BASE_URL || 'http://localhost:3000';
+
+    log.info(`
+Wuffle started on ${url}
+`);
+  }
+}
+
+async function run() {
+  await validate();
+  await start();
+  await open();
+}
+
+run().catch(err => {
+  log.error(err);
+
+  process.exit(1);
+});
