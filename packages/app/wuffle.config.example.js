dd0b56d1f897a58237c8a45200fb7b76a7dbb277
Nico Rehwaldt <git_nikku@nixis.de>
2019-07-17 21:33:39 +0200

    chore(app): document + update example wuffle.config.js

--- packages/app/wuffle.config.example.js
+++ packages/app/wuffle.config.example.js
@@ -1,10 +1,34 @@
+/**
+ * This defines all columns for the board as well as
+ * repositories to be synced.
+ *
+ *
+ * ## Columns
+ *
+ * Valid fields for columns are
+ *
+ *  * name       - unique name
+ *  * label      - label, if present on a GitHub issue column will be set
+ *  * closed     - if the column issues must be closed
+ *  * sorting    - true if column cards should be sorted
+ *                 incrementally based on links
+ *
+ * The default column is the column that holds open issues without
+ * any label constraints (Inbox in the example below).
+ *
+ *
+ * ## Repositories
+ *
+ * Repositories must be matched by their short name {owner}/{repoName}.
+ *
+ */
 module.exports = {
   columns: [
     { name: 'Inbox', label: null },
-    { name: 'Backlog', label: 'backlog' },
-    { name: 'Ready', label: 'ready' },
-    { name: 'In Progress', label: 'in progress' },
-    { name: 'Needs Review', label: 'needs review' },
+    { name: 'Backlog', label: 'backlog', sorting: true },
+    { name: 'Ready', label: 'ready', sorting: true },
+    { name: 'In Progress', label: 'in progress', sorting: true },
+    { name: 'Needs Review', label: 'needs review', sorting: true },
     { name: 'Done', label: null, closed: true }
   ],
   repositories: []
