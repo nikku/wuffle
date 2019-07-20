6ff6b2ce6d215db6e63ef10b2f4210fe74abb2ae
Nico Rehwaldt <git_nikku@nixis.de>
2019-07-20 23:46:58 +0200

    feat(apps/background-sync): sync all GitHub app installations
    This removes the duplicated maintainance of repositories in the
    wuffle.config.js as well as
    on GitHub.
    
    * print a warning if old config.repositories is encountered

--- packages/app/lib/apps/background-sync.js
+++ packages/app/lib/apps/background-sync.js
@@ -23,27 +23,17 @@ module.exports = async (app, config, store) => {
     name: 'wuffle:background-sync'
   });
 
-  const {
-    repositories
-  } = config;
 
-  if (repositories.length === 0) {
-    return log.error(
-      'must declare <config.repositories> to let wuffle know which repositories should be synched in background'
+  if ('repositories' in config) {
+
+    log.warn(`
+Configuring repositories via wuffle.config.js got removed, please update your config.
+
+We automatically synchronize all repositories you granted us access to via the GitHub app.
+`
     );
   }
 
-  function fetchRepository(repositoryName) {
-
-    const [ owner, repo ] = repositoryName.split('/');
-
-    return app.orgAuth(owner).then(github => {
-      return github.repos.get({
-        owner,
-        repo
-      });
-    }).then(res => res.data);
-  }
 
   async function applyUpdate(update) {
 
@@ -73,110 +63,167 @@ module.exports = async (app, config, store) => {
     return applyUpdate(filterIssue(issue, repository));
   }
 
-  async function syncRepositories(repositories, since) {
+  async function syncInstallation(installation, since) {
 
     const foundIssues = {};
 
-    // sync issues
-    for (const repositoryName of repositories) {
-      const [ owner, repo ] = repositoryName.split('/');
+    const owner = installation.account.login;
 
-      const params = {
-        sort: 'updated',
-        direction: 'desc',
-        owner,
-        repo
-      };
+    log.debug({ installation: owner }, 'sync start');
 
-      log.debug({ repositoryName }, 'issues sync start');
+    try {
+      const github = await app.orgAuth(owner);
 
-      try {
-        const github = await app.orgAuth(owner);
+      const repositories = await github.paginate(
+        github.apps.listRepos.endpoint.merge({ per_page: 100 }),
+        (response) => response.data
+      );
 
-        const [
-          open_issues,
-          closed_issues,
-          open_pull_requests,
-          closed_pull_requests,
-          repository
-        ] = await Promise.all([
+      for (const repository of repositories) {
 
-          // open issues
-          github.paginate(
-            github.issues.listForRepo.endpoint.merge({
-              ...params,
-              state: 'open'
-            }),
-            (response) => response.data.filter(issue => !issue.pull_request)
-          ),
+        const owner = repository.owner.login;
+        const repo = repository.name;
 
-          // closed issues, updated last 30 days
-          github.paginate(
-            github.issues.listForRepo.endpoint.merge({
-              ...params,
-              state: 'closed',
-              since: new Date(since).toISOString()
-            }),
-            (response) => response.data.filter(issue => !issue.pull_request)
-          ),
+        try {
 
-          // open pulls
-          github.paginate(
-            github.pulls.list.endpoint.merge({
-              ...params,
-              state: 'open'
-            }),
-            (response) => response.data
-          ),
+          log.debug({
+            owner,
+            repo
+          }, 'sync start');
 
-          // closed pulls, updated last 30 days
-          github.paginate(
-            github.pulls.list.endpoint.merge({
-              ...params,
-              state: 'closed'
-            }),
-            (response, done) => {
+          const params = {
+            sort: 'updated',
+            direction: 'desc',
+            owner,
+            repo
+          };
 
-              const pulls = response.data;
+          const [
+            open_issues,
+            closed_issues,
+            open_pull_requests,
+            closed_pull_requests
+          ] = await Promise.all([
 
-              const filtered = pulls.filter(pull => new Date(pull.updated_at).getTime() > since);
+            // open issues
+            github.paginate(
+              github.issues.listForRepo.endpoint.merge({
+                ...params,
+                state: 'open'
+              }),
+              (response) => response.data.filter(issue => !issue.pull_request)
+            ),
 
-              if (filtered.length !== pulls.length) {
-                done();
+            // closed issues, updated last 30 days
+            github.paginate(
+              github.issues.listForRepo.endpoint.merge({
+                ...params,
+                state: 'closed',
+                since: new Date(since).toISOString()
+              }),
+              (response) => response.data.filter(issue => !issue.pull_request)
+            ),
+
+            // open pulls
+            github.paginate(
+              github.pulls.list.endpoint.merge({
+                ...params,
+                state: 'open'
+              }),
+              (response) => response.data
+            ),
+
+            // closed pulls, updated last 30 days
+            github.paginate(
+              github.pulls.list.endpoint.merge({
+                ...params,
+                state: 'closed'
+              }),
+              (response, done) => {
+
+                const pulls = response.data;
+
+                const filtered = pulls.filter(pull => new Date(pull.updated_at).getTime() > since);
+
+                if (filtered.length !== pulls.length) {
+                  done();
+                }
+
+                return filtered;
               }
+            )
+          ]);
 
-              return filtered;
+          for (const issue of [ ...open_issues, ...closed_issues ]) {
+
+            try {
+              const {
+                id
+              } = await syncIssue(issue, repository);
+
+              // mark as found
+              foundIssues[id] = true;
+            } catch (error) {
+              log.debug({
+                owner,
+                repo,
+                issue: issue.number
+              }, 'sync failed', error);
             }
-          ),
+          }
 
-          // repository information
-          fetchRepository(repositoryName)
-        ]);
+          for (const pull_request of [ ...open_pull_requests, ...closed_pull_requests ]) {
 
-        for (const issue of [ ...open_issues, ...closed_issues ]) {
+            try {
+              const {
+                id
+              } = await syncPull(pull_request, repository);
 
-          const {
-            id
-          } = await syncIssue(issue, repository);
+              // mark as found
+              foundIssues[id] = true;
+            } catch (error) {
+              log.debug({
+                owner,
+                repo,
+                pr: pull_request.number
+              }, 'sync failed', error);
+            }
+          }
 
-          // mark as found
-          foundIssues[id] = true;
+          log.debug({
+            owner,
+            repo
+          }, 'sync completed');
+        } catch (error) {
+          log.warn({
+            owner,
+            repo
+          }, 'sync failed', error);
         }
 
-        for (const pull_request of [ ...open_pull_requests, ...closed_pull_requests ]) {
+      } // end --- for (const repository of repositories)
 
-          const {
-            id
-          } = await syncPull(pull_request, repository);
+      log.debug({ installation: owner }, 'sync completed');
+    } catch (error) {
+      log.warn({ installation: owner }, 'sync failed', error);
+    }
 
-          // mark as found
-          foundIssues[id] = true;
-        }
+    return foundIssues;
+  }
 
-        log.debug({ repositoryName }, 'issues sync completed');
-      } catch (error) {
-        log.warn({ repositoryName }, 'issues sync failed', error);
-      }
+  async function syncInstallations(installations, since) {
+
+    let foundIssues = {};
+
+    // sync issues
+    for (const installation of installations) {
+
+      const installationIssues = await syncInstallation(installation, since);
+
+      foundIssues = {
+        ...foundIssues,
+        ...installationIssues
+      };
     }
 
     return foundIssues;
@@ -217,7 +264,7 @@ module.exports = async (app, config, store) => {
     return expired;
   }
 
-  async function doSync(repositories) {
+  async function doSync(installations) {
 
     const now = Date.now();
 
@@ -229,7 +276,7 @@ module.exports = async (app, config, store) => {
     }, {});
 
     // synchronize existing issues
-    const foundIssues = await syncRepositories(repositories, Date.now() - syncLookback);
+    const foundIssues = await syncInstallations(installations, Date.now() - syncLookback);
 
     // check for all missing issues, these will
     // be automatically expired once they reach a
@@ -252,7 +299,10 @@ module.exports = async (app, config, store) => {
     log.info('start');
 
     try {
-      await doSync(repositories);
+
+      const installations = await app.getInstallations();
+
+      await doSync(installations);
 
       log.info('success');
     } catch (error) {
