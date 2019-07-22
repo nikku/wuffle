3078e5d23e6fbf7c69251ad99fd312bc500c247b
Nico Rehwaldt <git_nikku@nixis.de>
2019-07-22 21:00:16 +0200

    test(app): add test helper

---
+++ packages/app/test/helpers/index.js
@@ -0,0 +1,59 @@
+const {
+  AsyncInjector
+} = require('async-didi');
+
+function FakeLogger() {
+
+  this.log = function() {};
+  this.error = function() {};
+  this.info = function() {};
+  this.warn = function() {};
+  this.debug = function() {};
+
+  this.child = function() {
+    return this;
+  };
+}
+
+const fakeConfig = {
+  name: 'test',
+  columns: [
+    { name: 'Inbox', label: null },
+    { name: 'Backlog', label: 'backlog' },
+    { name: 'Ready', label: 'ready' },
+    { name: 'In Progress', label: 'in progress' },
+    { name: 'Needs Review', label: 'needs review' },
+    { name: 'Done', label: null, closed: true }
+  ]
+};
+
+const fakeModule = {
+  logger: [ 'type', FakeLogger ],
+  config: [ 'value', fakeConfig ]
+};
+
+async function bootstrap(options) {
+
+  const modules = [
+    fakeModule,
+    ...options.modules
+  ];
+
+  const injector = new AsyncInjector(modules);
+
+  // initialize modules ////////////
+
+  for (const module of modules) {
+
+    const init = module.__init__ || [];
+
+    for (const component of init) {
+      await injector[typeof component === 'function' ? 'invoke' : 'get'](component);
+    }
+
+  }
+
+  return injector;
+}
+
+module.exports.bootstrap = bootstrap;
