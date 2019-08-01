3b7c10b6b4ab5964bc9c5feb34415c7334c6adfa
Nico Rehwaldt <git_nikku@nixis.de>
2019-08-01 21:43:19 +0200

    chore(board): make className actually optional

--- packages/board/src/components/LinkIcon.svelte
+++ packages/board/src/components/LinkIcon.svelte
@@ -21,7 +21,7 @@
 <script>
   export let name;
   export let state = '';
-  export let className;
+  export let className = '';
 </script>
 
 {#if name === 'issue'}
@@ -52,11 +52,13 @@
 
 {/if}
 
+
 {#if name === 'linked-to'}
   <svg class="icon linked-to { className }" width="1rem" height="1rem" viewBox="0 0 14 16" version="1.1" aria-hidden="true" fill="currentColor">
     <path fill-rule="evenodd" d="M6.3 5.69a.942.942 0 0 1-.28-.7c0-.28.09-.52.28-.7.19-.18.42-.28.7-.28.28 0 .52.09.7.28.18.19.28.42.28.7 0 .28-.09.52-.28.7a1 1 0 0 1-.7.3c-.28 0-.52-.11-.7-.3zM8 7.99c-.02-.25-.11-.48-.31-.69-.2-.19-.42-.3-.69-.31H6c-.27.02-.48.13-.69.31-.2.2-.3.44-.31.69h1v3c.02.27.11.5.31.69.2.2.42.31.69.31h1c.27 0 .48-.11.69-.31.2-.19.3-.42.31-.69H8V7.98v.01zM7 2.3c-3.14 0-5.7 2.54-5.7 5.68 0 3.14 2.56 5.7 5.7 5.7s5.7-2.55 5.7-5.7c0-3.15-2.56-5.69-5.7-5.69v.01zM7 .98c3.86 0 7 3.14 7 7s-3.14 7-7 7-7-3.12-7-7 3.14-7 7-7z"></path>
   </svg>
 {/if}
+
 
 {#if name === 'epic'}
   <svg class="icon epic { className}" width="1rem" height="1rem" viewBox="0 0 12 16" version="1.1" aria-hidden="true" stroke="currentColor">
