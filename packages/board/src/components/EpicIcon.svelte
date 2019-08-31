34285c6e58fe89bbe83285a96d9169c2f768dc3b
Nico Rehwaldt <git_nikku@nixis.de>
2019-08-31 22:41:09 +0200

    fix(board): use em rather than rem for icon width
    Corrects display errors in Firefox (cf.
    https://github.com/nikku/wuffle/pull/48)

--- packages/board/src/components/EpicIcon.svelte
+++ packages/board/src/components/EpicIcon.svelte
@@ -7,7 +7,7 @@
   }
 </style>
 
-<svg class="icon" width="1rem" height="1rem" viewBox="0 0 12 16" version="1.1" aria-hidden="true">
+<svg class="icon" width="1em" height="1em" viewBox="0 0 12 16" version="1.1" aria-hidden="true">
 
   <path fill="currentColor" stroke="#1d76db" d="M1 15V4l5-3 5 3v11l-5-4-5 4z" stroke-width="1.5" />
 
