a1bf55c6c0b9629e10874195f9f9d287176d9cf1
Nico Rehwaldt <git_nikku@nixis.de>
2019-10-21 20:06:24 +0200

    chore(apps/github-issues): log not found warning with context

--- packages/app/lib/apps/github-issues/GithubIssues.js
+++ packages/app/lib/apps/github-issues/GithubIssues.js
@@ -81,7 +81,7 @@ function GithubIssues(logger, config, columns) {
       .catch(error => {
 
         // gracefully handle not found
-        log.error(params, 'not found');
+        log.warn(params, 'issue not found', error);
 
         return null;
       });
