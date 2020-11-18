a33abcbde46e468bdd6decad75cffae497eafa7a
Nico Rehwaldt <git_nikku@nixis.de>
2020-11-18 11:54:22 +0100

    feat(events-sync): support repository renaming
    Handle <repository.renamed> event and update stored issues accordingly.

--- packages/app/lib/apps/events-sync.js
+++ packages/app/lib/apps/events-sync.js
@@ -2,7 +2,9 @@ const {
   filterIssue,
   filterPull,
   filterIssueOrPull,
-  getIdentifier
+  filterRepository,
+  getIdentifier,
+  getKey
 } = require('../filters');
 
 
@@ -169,7 +171,25 @@ module.exports = function EventsSync(webhookEvents, store) {
   // https://docs.github.com/en/free-pro-team@latest/developers/webhooks-and-events/webhook-events-and-payloads#repository
 
   webhookEvents.on([
-    'repository.renamed',
+    'repository.renamed'
+  ], async ({ payload }) => {
+
+    const {
+      repository
+    } = payload;
+
+    await store.updateIssues(issue => {
+
+      if (issue.repository.id === repository.id) {
+        return {
+          key: getKey(issue, repository),
+          repository: filterRepository(repository)
+        };
+      }
+    });
+  });
+
+  webhookEvents.on([
     'repository.transferred'
   ], async ({ payload }) => {
 
