cb0a000c2e0df9544b2c8eeda095fb93db4f6420
Nico Rehwaldt <git_nikku@nixis.de>
2021-01-24 16:58:51 +0100

    feat: execute first time setup
    Starting wuffle for the first time initiates a setup process that
    guides the user through app configuration and GitHub app creation.
    
    In addition to the standard Probot setup flow we do the following:
    
    * validate mainfest configuration
    * proper success/completion message
    * fill required GITHUB_* environment variables
    * generate SESSION_SECRET
    
    First time setup is not execute if NODE_ENV=production.

--- packages/app/lib/probot/CustomProbot.js
+++ packages/app/lib/probot/CustomProbot.js
@@ -1,60 +1,77 @@
 const { Server, Probot } = require('probot');
 
 const { getLog } = require('probot/lib/helpers/get-log');
-const { setupAppFactory } = require('probot/lib/apps/setup');
+const { setupAppFactory } = require('./apps/setup');
+
+const { ManifestCreation } = require('./manifest-creation');
 
 const { getErrorHandler } = require('probot/lib/helpers/get-error-handler');
 const { getPrivateKey } = require('@probot/get-private-key');
 
 
-/**
- * A stub for proper programmatic usage of probot.
- *
- * @type {Object}
- */
-const CustomProbot = {
+async function run(appFn) {
 
-  async run(appFn) {
+  const appId = parseInt(process.env.APP_ID, 10);
+  const host = process.env.HOST;
+  const port = parseInt(process.env.PORT || '3000', 10);
+  const privateKey = getPrivateKey() || undefined;
 
-    const appId = parseInt(process.env.APP_ID, 10);
-    const host = process.env.HOST;
-    const port = parseInt(process.env.PORT || '3000', 10);
-    const privateKey = getPrivateKey() || undefined;
+  const log = getLog();
 
-    const log = getLog();
+  const server = new Server({
+    host,
+    log: log.child({ name: 'server' }),
+    port,
+    webhookPath: process.env.WEBHOOK_PATH,
+    webhookProxy: process.env.WEBHOOK_PROXY_URL,
+    Probot: Probot.defaults({
+      appId,
+      log: log.child({ name: 'probot' }),
+      privateKey,
+      secret: process.env.WEBHOOK_SECRET
+    })
+  });
 
-    const server = new Server({
-      host,
-      log: log.child({ name: 'wuffle:server' }),
-      port,
-      webhookPath: process.env.WEBHOOK_PATH,
-      webhookProxy: process.env.WEBHOOK_PROXY_URL,
-      Probot: Probot.defaults({
-        appId,
-        log: log.child({ name: 'wuffle:probot' }),
-        privateKey,
-        secret: process.env.WEBHOOK_SECRET
-      })
-    });
-
-    // use probots own setup app if the probot app
-    // is not configured yet
-    if (!appId || !privateKey) {
-      appFn = setupAppFactory(host, port);
-    }
-
-    // log all unhandled rejections
-    process.on('unhandledRejection', getErrorHandler(server.log));
-
-    // load apps
-    for (const fn of [ appFn ]) {
-      await server.load(fn);
-    }
-
-    await server.start();
-
-    return server;
+  // use probots own setup app if the probot app
+  // is not configured yet
+  if (!isSetup()) {
+    appFn = setupAppFactory(host, port);
   }
-};
 
-module.exports = CustomProbot;
+  // log all unhandled rejections
+  process.on('unhandledRejection', getErrorHandler(server.log));
+
+  // load apps
+  for (const fn of [ appFn ]) {
+    await server.load(fn);
+  }
+
+  await server.start();
+
+  return server;
+}
+
+function isSetup() {
+  const appId = parseInt(process.env.APP_ID, 10);
+  const privateKey = getPrivateKey() || undefined;
+
+  return appId && privateKey;
+}
+
+function validateSetup() {
+
+  const setup = new ManifestCreation();
+
+  const manifest = JSON.parse(setup.getManifest(setup.pkg));
+
+  return [
+    !manifest.url && new Error('No <url> configured in app.yml'),
+    !manifest.name && new Error('No <name> configured in app.yml')
+  ].filter(e => e);
+}
+
+module.exports = {
+  run,
+  isSetup,
+  validateSetup
+};
