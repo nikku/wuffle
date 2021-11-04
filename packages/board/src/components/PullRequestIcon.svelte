e134cb810a04097363336b6c6c079f9aa48f0b61
Nico Rehwaldt <git_nikku@nixis.de>
2021-11-04 09:42:17 +0100

    feat: color pull-request preview differently
    Use greyed out GitHub style for preview PRs.

--- packages/board/src/components/PullRequestIcon.svelte
+++ packages/board/src/components/PullRequestIcon.svelte
@@ -4,6 +4,7 @@
   $: merged = item.merged;
   $: open = item.state === 'open';
   $: closed = item.state === 'closed';
+  $: draft = item.draft || false;
 </script>
 
 <style lang="scss">
@@ -22,6 +23,10 @@
     &.merged {
       color: #6f42c1;
     }
+
+    &.draft {
+      color: #6e7781;
+    }
   }
 </style>
 
@@ -32,7 +37,7 @@
   </svg>
 
 {:else}
-  <svg class="pull-request-icon" class:closed={ closed } class:open={ open } width="1em" height="1em" viewBox="0 0 12 16" version="1.1" aria-hidden="true" fill="currentColor">
+  <svg class="pull-request-icon" class:closed={ closed } class:open={ open } class:draft={ draft } width="1em" height="1em" viewBox="0 0 12 16" version="1.1" aria-hidden="true" fill="currentColor">
     <path fill-rule="evenodd" d="M11 11.28V5c-.03-.78-.34-1.47-.94-2.06C9.46 2.35 8.78 2.03 8 2H7V0L4 3l3 3V4h1c.27.02.48.11.69.31.21.2.3.42.31.69v6.28A1.993 1.993 0 0 0 10 15a1.993 1.993 0 0 0 1-3.72zm-1 2.92c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zM4 3c0-1.11-.89-2-2-2a1.993 1.993 0 0 0-1 3.72v6.56A1.993 1.993 0 0 0 2 15a1.993 1.993 0 0 0 1-3.72V4.72c.59-.34 1-.98 1-1.72zm-.8 10c0 .66-.55 1.2-1.2 1.2-.65 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path>
   </svg>
 {/if}
