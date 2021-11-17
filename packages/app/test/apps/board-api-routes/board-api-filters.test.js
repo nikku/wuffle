4462d38961d85fc38884dad76db52b848f24f448
Nico Rehwaldt <git_nikku@nixis.de>
2021-11-17 21:34:16 +0100

    test(app): allow saving of fixtures

--- packages/app/test/apps/board-api-routes/board-api-filters.test.js
+++ packages/app/test/apps/board-api-routes/board-api-filters.test.js
@@ -7,11 +7,6 @@ const { expect } = require('chai');
 const fs = require('fs');
 
 
-function loadFixture(name) {
-  return JSON.parse(fs.readFileSync(__dirname + '/' + name + '.json', 'utf-8'));
-}
-
-
 describe('board-api-routes - board-api-filters', function() {
 
   it('should filter', function() {
@@ -28,3 +23,15 @@ describe('board-api-routes - board-api-filters', function() {
   });
 
 });
+
+
+// helpers /////////////
+
+function loadFixture(name) {
+  return JSON.parse(fs.readFileSync(__dirname + '/' + name + '.json', 'utf-8'));
+}
+
+// eslint-disable-next-line
+function saveFixture(name, contents) {
+  return fs.writeFileSync(__dirname + '/' + name + '.json', JSON.stringify(contents, null, 2), 'utf-8');
+}
