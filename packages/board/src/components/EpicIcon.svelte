5adaa8a39ea7283e79c8d0420c00cc4d386a3120
Nico Rehwaldt <git_nikku@nixis.de>
2022-11-30 23:28:21 +0100

    feat(board): overhaul icons
    Adapt to latest GitHub iconography + color scheme.

--- packages/board/src/components/EpicIcon.svelte
+++ packages/board/src/components/EpicIcon.svelte
@@ -1,14 +1,10 @@
 <style lang="scss">
 
   .icon {
-    margin-right: 2px;
+    margin-right: 3px;
 
     color: #1d76db;
   }
 </style>
 
-<svg class="icon" width="1em" height="1em" viewBox="0 0 12 16" version="1.1" aria-hidden="true">
-
-  <path fill="currentColor" stroke="#1d76db" d="M1 15V4l5-3 5 3v11l-5-4-5 4z" stroke-width="1.5" />
-
-</svg>
+<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="1em" height="1em" aria-hidden="true" fill="currentColor"><path d="M8.006 1.078a.748.748 0 0 0-.477.172L3.64 4.36l-.002.002-.088.07h.012c-.344.32-.561.773-.561 1.28v8.538a.75.75 0 0 0 1.227.578L8 11.723l3.773 3.105A.75.75 0 0 0 13 14.25V5.711c0-.527-.234-1-.604-1.32h-.002l-.01-.008a1.76 1.76 0 0 0-.075-.062L8.482 1.25a.748.748 0 0 0-.476-.173Z"/></svg>
