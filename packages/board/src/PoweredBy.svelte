a3f181ccf156e0ac34a2e15254b6ee218f06814c
Nico Rehwaldt <git_nikku@nixis.de>
2025-11-06 19:21:19 +0100

    fix(board): adjust powered by colors in dark mode

--- packages/board/src/PoweredBy.svelte
+++ packages/board/src/PoweredBy.svelte
@@ -20,7 +20,10 @@
      target="_blank" rel="noopener" title="About Wuffle">
 
     <!-- ./logo-gray.svg with dynamic fill color -->
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80" width="20" height="20"><path d="M12 0h56c7 0 12 5 12 12v56c0 7-5 12-12 12H12C5 80 0 75 0 68V12C0 5 5 0 12 0z" fill="currentColor"/><path d="M15 10h21c3 0 5 2 5 5v6c0 3-2 5-5 5H15c-2 0-4-2-4-5v-6c0-3 2-5 4-5zm0 20h21c3 0 5 2 5 5v6c0 2-2 4-5 4H15c-2 0-4-1-4-4v-6c0-3 2-5 4-5zm27 22h21c2 0 4 2 4 5v6c0 2-2 4-4 4H42c-2 0-4-2-4-4v-6c0-3 2-5 4-5z" fill="#fff"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80" width="20" height="20">
+      <path d="M12 0h56c7 0 12 5 12 12v56c0 7-5 12-12 12H12C5 80 0 75 0 68V12C0 5 5 0 12 0z" fill="currentColor"/>
+      <path class="logo-boxes" d="M15 10h21c3 0 5 2 5 5v6c0 3-2 5-5 5H15c-2 0-4-2-4-5v-6c0-3 2-5 4-5zm0 20h21c3 0 5 2 5 5v6c0 2-2 4-5 4H15c-2 0-4-1-4-4v-6c0-3 2-5 4-5zm27 22h21c2 0 4 2 4 5v6c0 2-2 4-4 4H42c-2 0-4-2-4-4v-6c0-3 2-5 4-5z" />
+    </svg>
   </a>
 </div>
 
@@ -49,8 +52,14 @@
       display: inline-flex;
     }
 
-    &:not(:hover, :focus-within) .logo {
-      color: #999;
+    &:not(:hover, :focus-within) {
+      .logo {
+        color: #999;
+      }
+
+      .logo-boxes {
+        fill: $light;
+      }
     }
 
     &:hover,
@@ -61,6 +70,10 @@
 
       .help {
         display: block;
+      }
+
+      .logo-boxes {
+        fill: $light;
       }
     }
 
@@ -91,7 +104,7 @@
       color: $light;
 
       &:not(:hover, :focus-within) .logo {
-        color: $dark;
+        color: lighten($dark, 30%);
       }
 
       &:hover,
