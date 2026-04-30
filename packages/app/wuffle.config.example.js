b926fb2ff06ac0d79d613f074046879f97544d0d
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-05-01 00:52:10 +0200

    docs: simplify board example configuration

--- packages/app/wuffle.config.example.js
+++ packages/app/wuffle.config.example.js
@@ -1,52 +1,36 @@
 /**
  * This defines board specific configuration.
- *
- * ### Name
- *
- * The name of your board, displayed in the board header.
- *
- *
- * ### Description
- *
- * Optional description of the board, will be served with the board front-end
- * for SEO purposes.
- *
- *
- * ### Columns
- *
- * Valid fields for columns are
- *
- *  * name       - unique name
- *  * label      - label, if present on a GitHub issue column will be set
- *  * closed     - if the column issues must be closed
- *  * sorting    - true if column cards should be sorted
- *                 incrementally based on links
- *  * fifo       - true to turn the default column ordering from
- *                 last in first out to first in first out
- *  * states     - a list of board states to map to this column
- *
- * The default column is the column that holds open issues without
- * any label constraints (Inbox in the example below).
- *
- *
- * ### Search Config
- *
- * You may define a default filter to apply to the board if
- * there is no user-defined search query.
- *
- * You may choose to ignore certain issues from connected repositories
- * through an ignore filter. Matching issues will not be added (saved) to
- * the board. If they matched previously (before an update) they will be
- * removed as part of an update.
- *
- * You may also configure whether bots are counted as
- * legitimate reviewers (for both review and approval).
  */
 export default {
 
+  /**
+   * The name of your board, displayed in the board header.
+   */
   name: 'My Wuffle Board',
+
+  /**
+   * (Optional) description of the board, will be served with the board
+   * front-end for SEO purposes.
+   */
   description: 'This board organizes all my tasks',
 
+  /**
+   * Definitions of columns to display on the board, and their state.
+   *
+   * Valid fields for columns are
+   *
+   *  * name       - unique name
+   *  * label      - label, if present on a GitHub issue column will be set
+   *  * closed     - if the column issues must be closed
+   *  * sorting    - true if column cards should be sorted
+   *                 incrementally based on links
+   *  * fifo       - true to turn the default column ordering from
+   *                 last in first out to first in first out
+   *  * states     - a list of board states to map to this column
+   *
+   * The default column is the column that holds open issues without
+   * any label constraints (Inbox in the example below).
+   */
   columns: [
     { name: 'Inbox', label: null },
     { name: 'Backlog', label: 'backlog', sorting: true },
@@ -56,9 +40,23 @@ export default {
     { name: 'Done', label: null, closed: true }
   ],
 
+  /**
+   * (Optional) default filter to apply to the board if there is no
+   * user-defined search query.
+   */
   defaultFilter: '!repo:"some/ignored-repository"',
 
+  /**
+   * (Optional) Filter matching issues that shall not be saved.
+   *
+   * Matching issues will not be added. If they were previously on the board
+   * they will be removed as part of an update.
+   */
   ignoreFilter: 'label:"invalid"',
 
+  /**
+   * Whether or not bots are treated as legitimate reviewers, for review and
+   * approval queries. Defaults to `false`.
+   */
   treatBotsAsReviewers: false
 };
